30a11d1915d23218eea64a0289e0350581d5610c
Zstzbxn <zstzbxn@pop-os.localdomain>
2024-02-12 17:29:08 +0100

    fetching one by one

--- app/Components/Cards.tsx
+++ app/Components/Cards.tsx
@@ -28,6 +28,7 @@ const Cards = ({ data, sortBy, order, filters }: CardsProps) => {
 
     // fetch here, and pass the data to the Card component
     const [cards, setCards] = React.useState<any>({})
+    const [loading, setLoading] = React.useState<boolean>(false)
 
     // React.useEffect(() => {
     //     if (!data) return;
@@ -109,52 +110,112 @@ const Cards = ({ data, sortBy, order, filters }: CardsProps) => {
         if (!data || _.isEqual(data, prevDataRef.current)) return;
         prevDataRef.current = data;
         setCards({})
-        data?.tokens.forEach((item: any, index: number) => {
-            fetch(`/api/metadata/${item}`)
-                .then(res => res.json())
-                .then(metadata => {
-                    fetch("https://api.oval3.game/graphql/", {
-                        "credentials": "omit",
-                        "headers": {
-                            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0",
-                            "Accept": "*/*",
-                            "Accept-Language": "en-US,en;q=0.5",
-                            "content-type": "application/json",
-                            "apollographql-client-name": "era2140-oval3",
-                            "apollographql-client-version": "0.0.1",
-                            "Access-Control-Allow-Origin": "*",
-                            "Sec-Fetch-Dest": "empty",
-                            "Sec-Fetch-Mode": "cors",
-                            "Sec-Fetch-Site": "same-site"
-                        },
-                        "referrer": "https://marketplace.oval3.game/",
-                        "body": "{\"operationName\":\"findCard\",\"variables\":{\"tokenId\":\"" + String(item) + "\",\"similarPlayer\":true},\"query\":\"query findCard($tokenId: String!, $similarPlayer: Boolean) {\\n  Card(tokenId: $tokenId, similarCard: $similarPlayer) {\\n    tokenId\\n    rarity\\n    owner {\\n      address\\n    }\\n    edition\\n    international\\n    academy\\n    similarPlayers {\\n      image\\n      tokenId\\n    }\\n    optaId\\n    season\\n    competition\\n    club {\\n      name\\n      clubCode\\n    }\\n    bidCount\\n    score\\n    image\\n    firstname\\n    lastname\\n    position\\n    age\\n    nationality\\n    listingStatus\\n    amount\\n    endTime\\n  }\\n}\"}",
-                        "method": "POST",
-                        "mode": "cors"
-                    })
-                        .then(res => res.json())
-                        .then(additional => {
-                            fetch(`https://score.oval3.game/api/scoring/player/${additional.data.Card.optaId}`)
-                                .then(res => res.json())
-                                .then(stats => {
-                                    setCards((prev: any) => {
-                                        return {
-                                            ...prev,
-                                            [item]: {
-                                                metadata: metadata.data,
-                                                additional: additional.data,
-                                                stats: stats.data
-                                            }
-                                        }
-                                    })
-                                });
-                        });
+        console.log("fetching cards", data.tokens.length)
+
+        // function bulkFetch() {
+        // data?.tokens.forEach(async (item: any, index: number) => {
+        //     fetch(`/api/metadata/${item}`)
+        //         .then(res => res.json())
+        //         .then(async metadata => {
+        //             if (metadata == undefined || metadata.error) return
+        //             fetch("https://api.oval3.game/graphql/", {
+        //                 "credentials": "omit",
+        //                 "headers": {
+        //                     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0",
+        //                     "Accept": "*/*",
+        //                     "Accept-Language": "en-US,en;q=0.5",
+        //                     "content-type": "application/json",
+        //                     "apollographql-client-name": "era2140-oval3",
+        //                     "apollographql-client-version": "0.0.1",
+        //                     "Access-Control-Allow-Origin": "*",
+        //                     "Sec-Fetch-Dest": "empty",
+        //                     "Sec-Fetch-Mode": "cors",
+        //                     "Sec-Fetch-Site": "same-site"
+        //                 },
+        //                 "referrer": "https://marketplace.oval3.game/",
+        //                 "body": "{\"operationName\":\"findCard\",\"variables\":{\"tokenId\":\"" + String(item) + "\",\"similarPlayer\":true},\"query\":\"query findCard($tokenId: String!, $similarPlayer: Boolean) {\\n  Card(tokenId: $tokenId, similarCard: $similarPlayer) {\\n    tokenId\\n    rarity\\n    owner {\\n      address\\n    }\\n    edition\\n    international\\n    academy\\n    similarPlayers {\\n      image\\n      tokenId\\n    }\\n    optaId\\n    season\\n    competition\\n    club {\\n      name\\n      clubCode\\n    }\\n    bidCount\\n    score\\n    image\\n    firstname\\n    lastname\\n    position\\n    age\\n    nationality\\n    listingStatus\\n    amount\\n    endTime\\n  }\\n}\"}",
+        //                 "method": "POST",
+        //                 "mode": "cors"
+        //             })
+        //                 .then(res => res.json())
+        //                 .then(async additional => {
+        //                     if (!additional || additional.errors ) return
+        //                     fetch(`https://score.oval3.game/api/scoring/player/${additional.data.Card.optaId}`)
+        //                         .then(res => res.json())
+        //                         .then(stats => {
+        //                             if (!stats || stats.error) return
+        //                             setCards((prev: any) => {
+        //                                 return {
+        //                                     ...prev,
+        //                                     [item]: {
+        //                                         metadata: metadata.data,
+        //                                         additional: additional.data,
+        //                                         stats: stats.data
+        //                                     }
+        //                                 }
+        //                             })
+        //                         });
+        //                 });
+        //         });
+        // });
+        // }
+
+        const fetchData = async () => {
+            setLoading(true);
+            for (const item of data.tokens) {
+                const res1 = await fetch(`/api/metadata/${item}`);
+                const metadata = await res1.json();
+                if (metadata == undefined || metadata.error) continue;
+
+                const res2 = await fetch("https://api.oval3.game/graphql/", {
+                    "credentials": "omit",
+                    "headers": {
+                        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0",
+                        "Accept": "*/*",
+                        "Accept-Language": "en-US,en;q=0.5",
+                        "content-type": "application/json",
+                        "apollographql-client-name": "era2140-oval3",
+                        "apollographql-client-version": "0.0.1",
+                        "Access-Control-Allow-Origin": "*",
+                        "Sec-Fetch-Dest": "empty",
+                        "Sec-Fetch-Mode": "cors",
+                        "Sec-Fetch-Site": "same-site"
+                    },
+                    "referrer": "https://marketplace.oval3.game/",
+                    "body": "{\"operationName\":\"findCard\",\"variables\":{\"tokenId\":\"" + String(item) + "\",\"similarPlayer\":true},\"query\":\"query findCard($tokenId: String!, $similarPlayer: Boolean) {\\n  Card(tokenId: $tokenId, similarCard: $similarPlayer) {\\n    tokenId\\n    rarity\\n    owner {\\n      address\\n    }\\n    edition\\n    international\\n    academy\\n    similarPlayers {\\n      image\\n      tokenId\\n    }\\n    optaId\\n    season\\n    competition\\n    club {\\n      name\\n      clubCode\\n    }\\n    bidCount\\n    score\\n    image\\n    firstname\\n    lastname\\n    position\\n    age\\n    nationality\\n    listingStatus\\n    amount\\n    endTime\\n  }\\n}\"}",
+                    "method": "POST",
+                    "mode": "cors"
                 });
-        });
+                const additional = await res2.json();
+                if (!additional || additional.errors) continue;
+                
+                const res3 = await fetch(`https://score.oval3.game/api/scoring/player/${additional.data.Card.optaId}`);
+                const stats = await res3.json();
+                if (!stats || stats.error) continue;
+                setCards((prev: any) => {
+                    return {
+                        ...prev,
+                        [item]: {
+                            metadata: metadata.data,
+                            additional: additional.data,
+                            stats: stats.data
+                        }
+                    }
+                });
+                if (data.tokens.indexOf(item) == data.tokens.length - 1) {
+                    setLoading(false);
+                }
+            }
+        };
+
+        fetchData();
+
     }, [data]);
 
     const sortData = (data: any) => {
-        const sorted = data.tokens.sort((a: any, b: any) => {
+        const sorted = data.sort((a: any, b: any) => {
+            a = a.metadata.token;
+            b = b.metadata.token;
             if (!cards[a] || !cards[b]) return 0
             if (sortBy === "id") {
                 if (order === "asc") {
@@ -244,17 +305,19 @@ const Cards = ({ data, sortBy, order, filters }: CardsProps) => {
         return true
     }
 
+
     return (
         <div className='w-full h-full mt-6'>
             <div className='w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 py-4 px-8 sm:px-12 md:px-24 xl:px-48 gap-12'>
-                {data && data.tokens && sortData(data).map((card: number, index: number) => (
+                {data && data.tokens && sortData(Object.values(cards)).map((card: any, index: number) => (
+                    card = card.metadata.token,
                     (cards[card] && isFiltered(cards[card]) &&
                         <div key={index} className="">
                             <Card metadata={cards[card].metadata} additionals={cards[card].additional} stats={cards[card].stats} />
                         </div>
                     )
                 ))}
-                {(Object.keys(cards).length > 0 && Object.keys(cards).length < data.tokens.length) && (
+                {loading && (Object.keys(cards).length > 0) && (
                     <div className='w-full h-full flex'>
                         <Image
                             src={"/images/card-placeholder.webp"}
@@ -267,7 +330,7 @@ const Cards = ({ data, sortBy, order, filters }: CardsProps) => {
                     </div>
                 )}
             </div>
-            {(Object.keys(cards).length == 0) && (
+            {loading && (Object.keys(cards).length == 0) && (
                 <RugbyLoader />
             )}
         </div>
